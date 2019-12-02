2e66dc991a3eb606f688b4e40f703cfa3519b775
Anastasia Eltsova <eltsova_ad@hotmail.com>
2019-12-02 11:45:39 +0300

    Пирамида и куб вращаются

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -1,180 +1,284 @@
-﻿// WindowsProject1.cpp : Определяет точку входа для приложения.
-//
+﻿#include "stdafx.h"
+#include <stdio.h>
+#include "GL\freeglut.h"
 
-#include "stdafx.h"
-#include "WindowsProject1.h"
+static HGLRC hRC;               // Постоянный контекст рендеринга
+static HDC hDC;                 // Приватный контекст устройства GDI
 
-#define MAX_LOADSTRING 100
+BOOL    keys[256];              // Массив для процедуры обработки клавиатуры
 
-// Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
-WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+GLfloat rtri=0;           // Угол для треугольник
+GLfloat rquad=30;          // Угол для четырехугольника
 
-// Отправить объявления функций, включенных в этот модуль кода:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int InitGL(GLsizei Width, GLsizei Height)    // Вызвать после создания окна GL
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);//очистка экрана в черный
+	glClearDepth(1.0);                      // Разрешить очистку буфера глубины
+	glDepthFunc(GL_LESS);                   // Тип теста глубины
+	glEnable(GL_DEPTH_TEST);                // разрешить тест глубины
+	glShadeModel(GL_SMOOTH);        // разрешить плавное цветовое сглаживание
+	glMatrixMode(GL_PROJECTION);    // Выбор матрицы проекции
+	glLoadIdentity();               // Сброс матрицы проекции
+	gluPerspective(45.0f, (GLfloat)Width / (GLfloat)Height, 0.1f, 100.0f);
+	// Вычислить соотношение геометрических размеров для окна
+	glMatrixMode(GL_MODELVIEW);     // Выбор матрицы просмотра модели
+	return TRUE;         // Инициализация прошла OK
 
-    // TODO: Разместите код здесь.
-
-    // Инициализация глобальных строк
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // Выполнить инициализацию приложения:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
-
-    MSG msg;
-
-    // Цикл основного сообщения:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
 }
 
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  ЦЕЛЬ: Регистрирует класс окна.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
+GLvoid ReSizeGLScene(GLsizei Width, GLsizei Height)
 {
-    WNDCLASSEXW wcex;
+	if (Height == 0)          // Предотвращение деления на ноль, если окно слишком мало
+		Height = 1;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	glViewport(0, 0, Width, Height);
+	// Сброс текущей области вывода и перспективных преобразований
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	glMatrixMode(GL_PROJECTION);            // Выбор матрицы проекций
+	glLoadIdentity();                       // Сброс матрицы проекции
 
-    return RegisterClassExW(&wcex);
+	gluPerspective(45.0f, (GLfloat)Width / (GLfloat)Height, 0.1f, 100.0f);
+	// Вычисление соотношения геометрических размеров для окна
+	glMatrixMode(GL_MODELVIEW);     // Выбор матрицы просмотра модели
 }
 
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
-//
-//   КОММЕНТАРИИ:
-//
-//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится главное окно программы.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+GLvoid DrawGLScene(GLvoid)
 {
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);// Очистка экрана и буфера глубины
+	glLoadIdentity();                               // Сброс просмотра
+	glTranslatef(-1.5f, 0.0f, -6.0f);                 // Сдвиг влево и вглубь экрана
+	glRotatef(rtri, 0.0f, 1.0f, 0.0f);                 // Вращение пирамиды по оси Y
+	glBegin(GL_TRIANGLES);                          // Начало рисования пирамиды
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	glColor3f(1.0f, 0.0f, 0.0f);                      // Красный
+	glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Передняя)
+	glColor3f(0.0f, 1.0f, 0.0f);                      // Зеленный
+	glVertex3f(-1.0f, -1.0f, 1.0f);                  // Левая точка
+	glColor3f(0.0f, 0.0f, 1.0f);                      // Синий
+	glVertex3f(1.0f, -1.0f, 1.0f);                  // Правая точка
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	glColor3f(1.0f, 0.0f, 0.0f);                      // Красная
+	glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Правая)
+	glColor3f(0.0f, 0.0f, 1.0f);                      // Синия
+	glVertex3f(1.0f, -1.0f, 1.0f);                  // Лево треугольника (Правая)
+	glColor3f(0.0f, 1.0f, 0.0f);                      // Зеленная
+	glVertex3f(1.0f, -1.0f, -1.0f);                 // Право треугольника (Правая)
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	glColor3f(1.0f, 0.0f, 0.0f);                      // Красный
+	glVertex3f(0.0f, 1.0f, 0.0f);                  // Низ треугольника (Сзади)
+	glColor3f(0.0f, 1.0f, 0.0f);                      // Зеленный
+	glVertex3f(1.0f, -1.0f, -1.0f);                 // Лево треугольника (Сзади)
+	glColor3f(0.0f, 0.0f, 1.0f);                      // Синий
+	glVertex3f(-1.0f, -1.0f, -1.0f);                 // Право треугольника (Сзади)
 
-   return TRUE;
+	glColor3f(1.0f, 0.0f, 0.0f);                      // Красный
+	glVertex3f(0.0f, 1.0f, 0.0f);                  // Верх треугольника (Лево)
+	glColor3f(0.0f, 0.0f, 1.0f);                      // Синий
+	glVertex3f(-1.0f, -1.0f, -1.0f);                  // Лево треугольника (Лево)
+	glColor3f(0.0f, 1.0f, 0.0f);                      // Зеленный
+	glVertex3f(-1.0f, -1.0f, 1.0f);                  // Право треугольника (Лево)
+	glEnd();                                                // Кончили рисовать пирамиду
+
+	glLoadIdentity();
+	glTranslatef(1.5f, 0.0f, -7.0f);          // Сдвинуть вправо и вглубь экрана
+	glRotatef(rquad, 1.0f, 1.0f, 1.0f);        // Вращение куба по X, Y & Z
+	glBegin(GL_QUADS);                      // Рисуем куб
+	glColor3f(0.0f, 1.0f, 0.0f);              // Синий
+	glVertex3f(1.0f, 1.0f, -1.0f);          // Право верх квадрата (Верх)
+	glVertex3f(-1.0f, 1.0f, -1.0f);          // Лево верх
+	glVertex3f(-1.0f, 1.0f, 1.0f);          // Лево низ
+	glVertex3f(1.0f, 1.0f, 1.0f);          // Право низ
+	glColor3f(1.0f, 0.5f, 0.0f);              // Оранжевый
+	glVertex3f(1.0f, -1.0f, 1.0f);          // Верх право квадрата (Низ)
+	glVertex3f(-1.0f, -1.0f, 1.0f);          // Верх лево
+	glVertex3f(-1.0f, -1.0f, -1.0f);          // Низ лево
+	glVertex3f(1.0f, -1.0f, -1.0f);          // Низ право
+	glColor3f(1.0f, 0.0f, 0.0f);              // Красный
+	glVertex3f(1.0f, 1.0f, 1.0f);          // Верх право квадрата (Перед)
+	glVertex3f(-1.0f, 1.0f, 1.0f);          // Верх лево
+	glVertex3f(-1.0f, -1.0f, 1.0f);          // Низ лево
+	glVertex3f(1.0f, -1.0f, 1.0f);          // Низ право
+	glColor3f(1.0f, 1.0f, 0.0f);              // Желтый
+	glVertex3f(1.0f, -1.0f, -1.0f);          // Верх право квадрата (Зад)
+	glVertex3f(-1.0f, -1.0f, -1.0f);          // Верх лево
+	glVertex3f(-1.0f, 1.0f, -1.0f);          // Низ лево
+	glVertex3f(1.0f, 1.0f, -1.0f);          // Низ право
+	glColor3f(0.0f, 0.0f, 1.0f);              // Синий
+	glVertex3f(-1.0f, 1.0f, 1.0f);          // Верх право квадрата (Лево)
+	glVertex3f(-1.0f, 1.0f, -1.0f);          // Верх лево
+	glVertex3f(-1.0f, -1.0f, -1.0f);          // Низ лево
+	glVertex3f(-1.0f, -1.0f, 1.0f);          // Низ право
+	glColor3f(1.0f, 0.0f, 1.0f);              // Фиолетовый
+	glVertex3f(1.0f, 1.0f, -1.0f);          // Верх право квадрата (Право)
+	glVertex3f(1.0f, 1.0f, 1.0f);          // Верх лево
+	glVertex3f(1.0f, -1.0f, 1.0f);          // Низ лево
+	glVertex3f(1.0f, -1.0f, -1.0f);          // Низ право
+	glEnd();                                // Закончили квадраты
+
+	rtri += 0.2f;             // Увеличим переменную вращения для треугольника 
+	rquad -= 0.15f;           // Уменьшим переменную вращения для квадрата 
+
 }
 
-//
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND  - обработать меню приложения
-//  WM_PAINT    - Отрисовка главного окна
-//  WM_DESTROY  - отправить сообщение о выходе и вернуться
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Разобрать выбор в меню:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+LRESULT CALLBACK WndProc(HWND    hWnd,
+	UINT    message,
+	WPARAM  wParam,
+	LPARAM  lParam) {
+	RECT    Screen;         // используется позднее для размеров окна
+	GLuint  PixelFormat;
+	static  PIXELFORMATDESCRIPTOR pfd =
+	{
+			sizeof(PIXELFORMATDESCRIPTOR),  // Размер этой структуры
+	1,                              // Номер версии (?)
+	PFD_DRAW_TO_WINDOW |            // Формат для Окна
+	PFD_SUPPORT_OPENGL |            // Формат для OpenGL
+	PFD_DOUBLEBUFFER,               // Формат для двойного буфера
+	PFD_TYPE_RGBA,                  // Требуется RGBA формат
+	16,                             // Выбор 16 бит глубины цвета
+	0, 0, 0, 0, 0, 0,                       // Игнорирование цветовых битов (?)
+	0,                              // нет буфера прозрачности
+	0,                              // Сдвиговый бит игнорируется (?)
+	0,                              // Нет буфера аккумуляции
+	0, 0, 0, 0,                             // Биты аккумуляции игнорируются (?)
+	16,                             // 16 битный Z-буфер (буфер глубины) 
+	0,                              // Нет буфера траффарета
+	0,                              // Нет вспомогательных буферов (?)
+	PFD_MAIN_PLANE,                 // Главный слой рисования
+	0,                              // Резерв (?)
+	0, 0, 0                         // Маски слоя игнорируются (?)
+	};
+
+	switch (message)        // Тип сообщения
+	{
+	case WM_CREATE:
+		hDC = GetDC(hWnd);      // Получить контекст устройства для окна
+		PixelFormat = ChoosePixelFormat(hDC, &pfd);
+		// Найти ближайшее совпадение для нашего формата пикселов
+		if (!PixelFormat)
+		{
+			MessageBox(0, L"Can't Find A Suitable PixelFormat.", L"Error", MB_OK | MB_ICONERROR);
+			PostQuitMessage(0);
+			// Это сообщение говорит, что программа должна завершится
+			break;  // Предтовращение повтора кода
+		}
+		if (!SetPixelFormat(hDC, PixelFormat, &pfd))
+		{
+			MessageBox(0, L"Can't Set The PixelFormat.", L"Error", MB_OK | MB_ICONERROR);
+			PostQuitMessage(0);
+			break;
+		}
+		hRC = wglCreateContext(hDC);
+		if (!hRC)
+		{
+			MessageBox(0, L"Can't Create A GL Rendering Context.", L"Error", MB_OK | MB_ICONERROR);
+			PostQuitMessage(0);
+			break;
+		}
+		if (!wglMakeCurrent(hDC, hRC))
+		{
+			MessageBox(0, L"Can't activate GLRC.", L"Error", MB_OK | MB_ICONERROR);
+			PostQuitMessage(0);
+			break;
+		}
+		GetClientRect(hWnd, &Screen);
+		InitGL(Screen.right, Screen.bottom);
+		break;
+	case WM_DESTROY:
+	case WM_CLOSE:
+		ChangeDisplaySettings(NULL, 0);
+
+		wglMakeCurrent(hDC, NULL);
+		wglDeleteContext(hRC);
+		ReleaseDC(hWnd, hDC);
+
+		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		keys[wParam] = TRUE;
+		break;
+	case WM_KEYUP:
+		keys[wParam] = FALSE;
+		break;
+	case WM_SIZE:
+		ReSizeGLScene(LOWORD(lParam), HIWORD(lParam));
+		break;
+	default:
+		return (DefWindowProc(hWnd, message, wParam, lParam));
+	}
+	return (0);
 }
 
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	MSG             msg;    // Структура сообщения Windows
+	WNDCLASS        wc; // Структура класса Windows для установки типа окна
+	HWND            hWnd;   // Сохранение дискриптора окна
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = L"OpenGL WinClass";
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"Failed To Register The Window Class.", L"Error", MB_OK | MB_ICONERROR);
+		return FALSE;
+	}
+	hWnd = CreateWindow(
+		L"OpenGL WinClass",
+		L"Jeff Molofee's GL Code Tutorial ... NeHe '99", // Заголовок вверху окна
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+		WS_POPUP |
+		WS_CLIPCHILDREN |
+		WS_CLIPSIBLINGS,
+
+		0, 0,                   // Позиция окна на экране
+		640, 480,               // Ширина и высота окна
+
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	if (!hWnd)
+	{
+		MessageBox(0, L"Window Creation Error.", L"Error", MB_OK | MB_ICONERROR);
+		return FALSE;
+	}
+	DEVMODE dmScreenSettings;                       // Режим работы
+
+	memset(&dmScreenSettings, 0, sizeof(DEVMODE));          // Очистка для хранения установок
+	dmScreenSettings.dmSize = sizeof(DEVMODE);              // Размер структуры Devmode
+	dmScreenSettings.dmPelsWidth = 640;                  // Ширина экрана
+	dmScreenSettings.dmPelsHeight = 480;                  // Высота экрана
+	dmScreenSettings.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT; // Режим Пиксела
+	ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN);
+	// Переключение в полный экран
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	SetFocus(hWnd);
+	while (1)
+	{
+		// Обработка всех сообщений
+		while (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (GetMessage(&msg, NULL, 0, 0))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else
+			{
+				return TRUE;
+			}
+		}
+		DrawGLScene();                          // Нарисовать сцену
+		SwapBuffers(hDC);                               // Переключить буфер экрана
+
+		if (keys[VK_ESCAPE]) SendMessage(hWnd, WM_CLOSE, 0, 0);    // Если ESC - выйти
+	}
 }
